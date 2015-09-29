9ff4294044f79e5f650a0769065b12c9e80d65ea
philfanzhou <philfanzhou@hotmail.com>
2015-09-29 15:31:38 +0800

    迁移框架代码库

---
+++ 5_Infrastructure/Framework.Infrastructure.Container/ConstructorParameter.cs
@@ -0,0 +1,8 @@
+﻿namespace Framework.Infrastructure.Container
+{
+    public class ConstructorParameter
+    {
+        public string Name { get; set; }
+        public object Value { get; set; }
+    }
+}
---
+++ 5_Infrastructure/Framework.Infrastructure.MemoryMappedFile/IMemoryMappedFileHeader.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Framework.Infrastructure.MemoryMappedFile
+{
+    public interface IMemoryMappedFileHeader
+    {
+        /// <summary>
+        /// 当前已有数据量
+        /// </summary>
+        int DataCount { get; set; }
+
+        /// <summary>
+        /// 最大可容纳的数据量
+        /// </summary>
+        int MaxDataCount { get; set; }
+    }
+}
