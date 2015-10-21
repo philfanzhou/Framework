f844b5bad13d6e95fba0f8692a0ac390b390dc23
philfanzhou <philfanzhou@hotmail.com>
2015-10-21 18:04:48 +0800

    调整测试代码，为性能测试准备

---
+++ 6_Test/Test.Infrastructure.MemoryMap/Performance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Test.Infrastructure.MemoryMap
+{
+    public partial class MemoryMappedFileTest
+    {
+        [TestMethod]
+        public void TestPerformance()
+        {
+            int dataCount = 100000;
+            string path = CreateFileAnyway("PerformanceTest.dat", dataCount);
+
+            List<DataItem> expectedList = CreateDataItem(dataCount);
+        }
+    }
+}
