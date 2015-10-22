cf5ad64674d6b44eac4eec8a71ffb43a8980a256
philfanzhou <philfanzhou@hotmail.com>
2015-10-22 22:53:42 +0800

    建立了简单的同步访问框架

---
+++ 5_Infrastructure/Framework.Infrastructure.MemoryMap/ConcurrentFile.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Framework.Infrastructure.MemoryMap
+{
+    public class ConcurrentFile<TDataHeader, TDataItem>
+        : NonConcurrentMemoryMappedFile<TDataHeader, TDataItem>
+        where TDataHeader : struct, IMemoryMappedFileHeader
+        where TDataItem : struct
+    {
+        private readonly ReaderWriterLock _rwLock = new ReaderWriterLock();
+        private readonly TimeSpan _timeOut = new TimeSpan(0, 0, 20); 
+
+        #region Constructor
+
+        /// <summary>
+        /// 打开文件调用的构造函数
+        /// </summary>
+        /// <param name="path"></param>
+        protected ConcurrentFile(string path) : base(path)
+        { }
+
+        /// <summary>
+        /// 创建文件调用的构造函数
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="maxDataCount"></param>
+        protected ConcurrentFile(string path, TDataHeader fileHeader)
+            : base(path, fileHeader)
+        { }
+        #endregion
+
+        //internal bool IsAlive
+        //{
+        //    get { return _rwLock.IsReaderLockHeld}
+        //}
+
+        protected override IEnumerable<TDataItem> DoRead(int index, int count)
+        {
+            _rwLock.AcquireReaderLock(_timeOut);
+            var result = base.DoRead(index, count);
+            _rwLock.ReleaseReaderLock();
+
+            return result;
+        }
+
+        protected override void DoDelete(int index, int count)
+        {
+            _rwLock.AcquireWriterLock(_timeOut);
+            base.DoDelete(index, count);
+            _rwLock.ReleaseWriterLock();
+        }
+
+        protected override void DoInsert(IEnumerable<TDataItem> items, int index, bool ChangeDataCount)
+        {
+            _rwLock.AcquireWriterLock(_timeOut);
+            base.DoInsert(items, index, ChangeDataCount);
+            _rwLock.ReleaseWriterLock();
+        }
+    }
+}
---
+++ 5_Infrastructure/Framework.Infrastructure.MemoryMap/ConcurrentFileFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Infrastructure.MemoryMap
+{
+    public abstract class ConcurrentFileFactory<TFile, TDataHeader, TDataItem>
+            where TFile : ConcurrentFile<TDataHeader, TDataItem>
+            where TDataHeader : struct, IMemoryMappedFileHeader
+            where TDataItem : struct
+    {
+        private readonly ConcurrentDictionary<string, TFile> _filePool
+            = new ConcurrentDictionary<string, TFile>();
+
+        //public TFile Open(string path)
+        //{
+        //    if(_filePool.ContainsKey(path))
+        //    {
+        //        return _filePool[path];
+        //    }
+        //    else
+        //    {
+        //        // 可能多个线程同时进入这里
+        //        _filePool.TryAdd(path, DoOpen(path));
+        //    }
+        //}
+
+        public abstract TFile DoOpen(string path);
+
+        public abstract TFile Create(string path, TDataHeader fileHeader);
+    }
+}
